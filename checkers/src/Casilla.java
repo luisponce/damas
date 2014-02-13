8c2bc2a4f9a7d947ba144b0b268566d9a9aa052e
luis M Ponce <luisponcekun@gmail.com>
2014-02-13 11:01:19 -0500

    added Pos, Casilla and Tablero clases

---
+++ checkers/src/Casilla.java
@@ -0,0 +1,32 @@
+
+
+/**
+ *
+ * @author luisponcedeleon
+ */
+public enum Casilla {
+   /**
+    * EMPTY: casilla vacia
+    */
+    E,
+    
+    /**
+     * BLACK: Casilla con ficha negra
+     */
+    B,
+    
+    /**
+     * WHITE: Casilla con ficha blanca
+     */
+    W, 
+    
+    /**
+     * BLACK QUEEN: Casilla con ficha negra coronada
+     */
+    BC,
+    
+    /**
+     * WHITE QUEEN: Casilla con ficha blanca coronada
+     */
+    WC 
+}
---
+++ checkers/src/Tablero.java
@@ -0,0 +1,47 @@
+
+/**
+ *
+ * 
+ * @author luisponcedeleon
+ */
+public class Tablero {
+    /**
+     * Tablero de juego, de 8 por 8, con el estado de cada casilla.
+     */
+    private Casilla[][] board = new Casilla[8][8];
+    
+    /**
+     * Metodo para duplicar el tablero.
+     * 
+     * @return Una copia del tablero.
+     */
+    public Tablero Duplicar(){
+        
+        return null;
+    }
+    
+    /**
+     * Metodo para mover una ficha de la posicion Inical a la posicion Final.
+     * No valida si el movimiento es valido, solo realiza el movimiento.
+     * 
+     * @param posI Posicion Inicial de la ficha.
+     * @param posF Posicion Final de la ficha.
+     */
+    public void Mover(Pos posI, Pos posF){
+        
+    }
+    
+    /**
+     * Metodo para validar un movimiento desde posicion inicial a posicion final
+     * 
+     * @param Posicion Inicial de la ficha.
+     * @param posF Posicion Final de la ficha.
+     * @param esTurnoAI True si es el turno de la AI, false de lo contrario.
+     * @return True si es valido, False si no lo es.
+     */
+    public boolean Validar(Pos posI, Pos posF, boolean esTurnoAI){
+        
+        return true;
+    }
+    
+}
