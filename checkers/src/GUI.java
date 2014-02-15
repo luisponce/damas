1e6508ae82cb5c12d22dc009800596029a7d0601
ALICIA PRATS <ALICIA PRATS@ALICIAPRATS-PC>
2014-02-14 23:36:48 -0500

    Paneles GUI, metodo para matriz estructurado pero no implementado

--- checkers/src/GUI.java
+++ checkers/src/GUI.java
@@ -1,19 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.awt.Color;
 
 /**
- *
- * @author carcosp
+ * Interfaz Grafica
  */
 public class GUI extends javax.swing.JFrame {
 
-    /**
-     * Creates new form GUI
-     */
     public GUI() {
         initComponents();
+        
     }
 
     /**
@@ -62,15 +57,18 @@ public class GUI extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel3.setPreferredSize(new java.awt.Dimension(400, 400));
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 398, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 398, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -124,7 +122,7 @@ public class GUI extends javax.swing.JFrame {
                     .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -141,10 +139,30 @@ public class GUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    private void crearMatriz() {
+        float dimensionX = jPanel3.getAlignmentX();
+        float dimensionY = jPanel3.getAlignmentY();
+        float sizeX = dimensionX/8;
+        float sizeY = dimensionY/8;
+        int tamañoX = (int)sizeX;
+        int tamañoY = (int)sizeY;
+        for (int i = 1; i<=8; i++) {
+            for (int j = 1; j<=8; j++) {
+                if ((i+j) % 2 == 0) {
+                    javax.swing.JPanel jPanelNew = new javax.swing.JPanel();
+                    jPanelNew.setBackground(Color.white);
+                    jPanelNew.setSize(tamañoX, tamañoY);
+                    jPanel3.add(jPanelNew, tamañoX*i);                    
+                } else {
+                    javax.swing.JPanel jPanelNew = new javax.swing.JPanel();
+                    jPanelNew.setBackground(Color.black);
+                    jPanelNew.setSize(tamañoX, tamañoY);
+                    jPanel3.add(jPanelNew, tamañoX*i);
+                }
+            } 
+        }
+    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -175,6 +193,7 @@ public class GUI extends javax.swing.JFrame {
                 new GUI().setVisible(true);
             }
         });
+        
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
